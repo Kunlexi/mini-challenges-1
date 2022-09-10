dad62306dba586b63f2b4f1948571c54f00045ab
Kunlexi <91717420+Kunlexi@users.noreply.github.com>
2022-09-10 21:07:10 +0100

    roman numeral update

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -1,9 +1,27 @@
-/**
- * Implement the solution to brackets
- *
- * @param {string} str The string of brackets.
- * @returns {"valid" | "invalid"} Whether or not the string is valid.
- */
-function isValid(str) {}
+let correct = 'my st(ring) [ok] {hello}';
+let uncorrect = 'my st(ring) [o(k] {hello}';
 
-module.exports = isValid;
+syntaxChecker = (str) => {
+  let stack = [];
+  // For each char in the string
+  [...str].forEach(c => {
+    // opened char ? add to the stack
+    if ('([{'.includes(c)){
+      stack.push(c);
+    }
+    // closed char ? check if match the last opened
+    else if (')]}'.includes(c)){
+      let top = stack.pop();
+      if (! (c === ')' && top === '('
+          || c === ']' && top === '['
+          || c === '}' && top === '{')){
+        stack.push(c);
+      }
+    }
+  });
+  // stack empty = correct
+  return stack.length === 0;
+}
+
+console.log(syntaxChecker(correct)); // true
+console.log(syntaxChecker(uncorrect)); // false
--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -1,9 +1,31 @@
-/**
- * Implement the solution in this function
- *
- * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
- * @returns {number} The decimal equivalent.
- */
-function romanToDecimal(roman) {}
-
-module.exports = romanToDecimal;
+function convertToRoman(num) {
+    var roman = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+    }
+    var result = '';
+    for (var key in roman) {
+      if (num == roman[key]) {
+        return result +=key;
+      }
+      var check = num > roman[key];
+      if(check) {
+        result = result + key.repeat(parseInt(num/roman[key]));
+        num = num%roman[key];
+      }
+    }
+   return result;
+  }
+  
+  console.log(convertToRoman(3999));
